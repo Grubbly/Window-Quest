b41a28c92f54d6153d18a31c70ef089ca49689d6
unknown <orthogonaltrajectory@gmail.com>
2018-10-26 21:44:47 -0800

    8 characters and transitioning to terrain
    Characters: 3 Lumberjacks, 1 butterfly, 1 squirrel, 1 spookyboy, 1 imp, 1 guard -- still need 2 characters

---
+++ Window Quest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 100;
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed);
+	}
+}
---
+++ Window Quest/Assets/Scripts/bounce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour {
+
+    public float speed = 0.1f;
+    public int max = 50;
+    private Transform _transform;
+    private float height;
+    private int count = 0;
+
+    private void Awake()
+    {
+        _transform = gameObject.GetComponent<Transform>();
+        height = _transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (count < max) {
+            _transform.Translate(new Vector3(0, speed, 0));
+            count++;
+        }
+        else if(count >= max) {
+            _transform.Translate(new Vector3(0, -speed, 0));
+            count++;
+        }
+        if(count == 2*max) {
+            count = 0;
+        }
+	}
+}
